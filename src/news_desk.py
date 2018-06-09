2990297407ee5e0a4419080c6423a5665dedd8a1
Jack Bonatakis <jackbonatakis@gmail.com>
2018-06-09 15:00:29 -0600

    Handle user entering invalid key

--- src/news_desk.py
+++ src/news_desk.py
@@ -15,134 +15,156 @@ import set_key
 #import pull_text
 
 class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
 
-# Check for api key, saved in `~/.nd_config/key` file. If doesn't exist, prompt for key and save in `~/.nd_config/key` file
-home = os.path.expanduser('~')
-try:
-    key_file = open(home + '/.nd_config/key', 'r')
-    key = key_file.readline()
-except FileNotFoundError:
-    set_key.set_key()
-    key_file = open(home + '/.nd_config/key', 'r')
-    key = key_file.readline()
+# # Check for api key, saved in `~/.nd_config/key` file. If doesn't exist, prompt for key and save in `~/.nd_config/key` file
+# home = os.path.expanduser('~')
+# try:
+# 	key_file = open(home + '/.nd_config/key', 'r')
+# 	key = key_file.readline()
+# except FileNotFoundError:
+# 	set_key.set_key()
+# 	key_file = open(home + '/.nd_config/key', 'r')
+# 	key = key_file.readline()
 
-url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key)
+# url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key)
 
 
 url_list = []
 
 class News_desk():
 
-    # Parse JSON and print Title, Description, and source, then get next steps from user
-    def get_articles(self):
-        data = requests.get(url).json()
-        for i, article in enumerate(data['articles']):
-            cprint('\n' + str(i+1) +  '. ' +  article['title'],'blue', attrs=['bold'])
-            if len(str(article['description'])) < 5:# == '' or article['description'] in ['None', 'none']:
-                pass
-            else:
-                print(article['description'])
-            print('Source: ' + article['source']['name'])
-            url_list.append(article['url'])
+	def check_key(self):
+		# Check for api key, saved in `~/.nd_config/key` file. If doesn't exist, prompt for key and save in `~/.nd_config/key` file
+		home = os.path.expanduser('~')
+		try:
+			key_file = open(home + '/.nd_config/key', 'r')
+			key = key_file.readline()
+		except FileNotFoundError:
+			set_key.set_key()
+			key_file = open(home + '/.nd_config/key', 'r')
+			key = key_file.readline()
 
-        # Prompt user for selection to determine next steps
-        print(color.GREEN + color.BOLD +"\nEnter 'r' to refresh the articles, or 'q' to quit the program. \nSelect an article to read.\n" + color.END)
+		url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key)
+		news.get_articles(url)
 
-        choice = input('>> ')
-        selection = str(choice).lower()
+	# Parse JSON and print Title, Description, and source, then get next steps from user
+	def get_articles(self, url):
+		data = requests.get(url).json()
+		try:
+			for i, article in enumerate(data['articles']):
+				cprint('\n' + str(i+1) +  '. ' +  article['title'],'blue', attrs=['bold'])
+				if len(str(article['description'])) < 5:# == '' or article['description'] in ['None', 'none']:
+					pass
+				else:
+					print(article['description'])
+				print('Source: ' + article['source']['name'])
+				url_list.append(article['url'])
+		except KeyError:
+			home = os.path.expanduser('~')
+			set_key.set_key()
+			key_file = open(home + '/.nd_config/key', 'r')
+			key = key_file.readline()
+			url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + key)
+			news.get_articles(url)
 
-        # Valid input commands
-        refresh = ["r", "ref", "refresh"]
-        exit = ["e", "exit", "q", "quit"]
+		# Prompt user for selection to determine next steps
+		print(color.GREEN + color.BOLD +"\nEnter 'r' to refresh the articles, or 'q' to quit the program. \nSelect an article to read.\n" + color.END)
 
-        # Call 'options' function
-        news.options(selection)
+		choice = input('>> ')
+		selection = str(choice).lower()
 
+		# Valid input commands
+		refresh = ["r", "ref", "refresh"]
+		exit = ["e", "exit", "q", "quit"]
 
-    def options(self, selection):
-
-        refresh = ["r", "ref", "refresh"]
-        exit = ["e", "exit", "q", "quit"]
-
-        while True:
-            if "-b" in sys.argv:
-                try:
-                    if isinstance(int(selection), int):
-                        if int(selection) <= len(url_list) and int(selection) > 0:
-                            print("\n")
-                            news.open_link(int(selection))
-                            news.try_again()
-                        else:
-                            news.try_again()
-                except ValueError:
-                    if selection in refresh:
-                        url_list.clear()
-                        news.get_articles()
-                    elif selection in exit:
-                        sys.exit(0)
-                    else:
-                        news.try_again()
-            else:
-                try:
-                    if isinstance(int(selection), int):
-                        if int(selection) <= len(url_list) and int(selection) > 0:
-                            print("\n")
-                            news.get_text(int(selection))
-                            news.try_again()
-                        else:
-                            news.try_again()
-                except ValueError:
-                    if selection in refresh:
-                        url_list.clear()
-                        news.get_articles()
-                    elif selection in exit:
-                        sys.exit(0)
-                    else:
-                        news.try_again()
+		# Call 'options' function
+		news.options(selection)
 
 
-    # Bounces back and forth between news.options() and news.try_again() until user selects valid command. Probably a better way to do this somehow.
-    # Idea: limit number of incorrect commands. After 5? 10? quit program
-    def try_again(self):
-        selection = str(input(color.GREEN + color.BOLD +"\nPlease enter 'r' to refresh the articles, or 'q' to quit the program.\nSelect an article to read." + color.END + "\n\n>> "))
-        news.options(selection)
+	def options(self, selection):
+
+		refresh = ["r", "ref", "refresh"]
+		exit = ["e", "exit", "q", "quit"]
+
+		while True:
+			if "-b" in sys.argv:
+				try:
+					if isinstance(int(selection), int):
+						if int(selection) <= len(url_list) and int(selection) > 0:
+							print("\n")
+							news.open_link(int(selection))
+							news.try_again()
+						else:
+							news.try_again()
+				except ValueError:
+					if selection in refresh:
+						url_list.clear()
+						news.get_articles()
+					elif selection in exit:
+						sys.exit(0)
+					else:
+						news.try_again()
+			else:
+				try:
+					if isinstance(int(selection), int):
+						if int(selection) <= len(url_list) and int(selection) > 0:
+							print("\n")
+							news.get_text(int(selection))
+							news.try_again()
+						else:
+							news.try_again()
+				except ValueError:
+					if selection in refresh:
+						url_list.clear()
+						news.get_articles()
+					elif selection in exit:
+						sys.exit(0)
+					else:
+						news.try_again()
 
 
-    def open_link(self, choice):
-        if choice <= len(url_list)+1:
-            counter = 0
-            while counter < 1:
-                pyperclip.copy(url_list[choice-1])
-                webbrowser.open_new_tab(url_list[choice-1])
-                counter += 1
-        else:
-            news.try_again()
+	# Bounces back and forth between news.options() and news.try_again() until user selects valid command. Probably a better way to do this somehow.
+	# Idea: limit number of incorrect commands. After 5? 10? quit program
+	def try_again(self):
+		selection = str(input(color.GREEN + color.BOLD +"\nPlease enter 'r' to refresh the articles, or 'q' to quit the program.\nSelect an article to read." + color.END + "\n\n>> "))
+		news.options(selection)
 
-    def get_text(self, choice):
-        if choice <= len(url_list)+1:
-            counter = 0
-            while counter < 1:
-                #pull_text.pull_text(url_list[choice-1])
-                article = Article(url_list[choice-1])
-                article.download()
-                article.parse()
-                print(color.BLUE + color.BOLD + article.title + color.END)
-                print(article.text)
-                counter += 1
+
+	def open_link(self, choice):
+		if choice <= len(url_list)+1:
+			counter = 0
+			while counter < 1:
+				pyperclip.copy(url_list[choice-1])
+				webbrowser.open_new_tab(url_list[choice-1])
+				counter += 1
+		else:
+			news.try_again()
+
+	def get_text(self, choice):
+		if choice <= len(url_list)+1:
+			counter = 0
+			while counter < 1:
+				#pull_text.pull_text(url_list[choice-1])
+				article = Article(url_list[choice-1])
+				article.download()
+				article.parse()
+				print(color.BLUE + color.BOLD + article.title + color.END)
+				print(article.text)
+				counter += 1
 
 
 
 news = News_desk()
 
 if __name__ == "__main__":
-    news.get_articles()
+	news.check_key()
--- src/set_key.py
+++ src/set_key.py
@@ -7,15 +7,21 @@ def set_key():
 	try:
 		os.makedirs(os.path.join(home, '.nd_config'))
 		f = open(home +'/.nd_config/key', 'w')
-		key = str(input("Enter newsapi.org key here >> ")).strip()
-		f.write(key)
-		f.close()
-	except FileExistsError:
-		f = open(home + '/.nd_config/key', 'w')
-                print("Welcome to News Desk! You need an API key. Please point your browser to https://newsapi.org, register for a free developer account, and copy your API key.")
-		print("Once you complete this process you will not need to repeat it unless you regenerate you API key or delete the key file.")
+		print("Welcome to News Desk! You need an API key. Please point your browser to https://newsapi.org, register for a free developer account, and copy your API key.")
+		print("Once you complete this process you will not need to repeat it unless you regenerate your API key or delete the key file.")
 		key = str(getpass.getpass("Enter newsapi.org key here >> ")).strip()
 		f.write(key)
 		f.close()
 		print("Key saved to ~/.nd_config/key. Gathering your news now...")
+		print("\n")
+		time.sleep(5)
+
+	except FileExistsError:
+		f = open(home + '/.nd_config/key', 'w')
+		print("Your API key is invalid. Please point your browser to https://newsapi.org, confirm your API key, and enter it below.")
+		key = str(getpass.getpass("Enter newsapi.org key here >> ")).strip()
+		f.write(key)
+		f.close()
+		print("Key saved to ~/.nd_config/key. Gathering your news now...")
+		print("\n")
 		time.sleep(5)
