10ae4327bbff23912e01bc0f50e022fe075e7bd9
Jack Bonatakis <jackbonatakis@gmail.com>
2018-06-09 13:48:41 -0600

    Utilize getpass and add time delay after inserting key

--- src/set_key.py
+++ src/set_key.py
@@ -1,4 +1,6 @@
 import os
+import getpass
+import time
 
 def set_key():
 	home = os.path.expanduser('~')
@@ -12,7 +14,8 @@ def set_key():
 		f = open(home + '/.nd_config/key', 'w')
 		print("Welcome to News Desk! You need an API key. Please point your browser to www.newsapi.org, register for a free developer account, and copy your API key.")
 		print("Once you complete this process you will not need to repeat it unless you regenerate you API key or delete the key file.")
-		key = str(input("Enter newsapi.org key here >> ")).strip()
+		key = str(getpass.getpass("Enter newsapi.org key here >> ")).strip()
 		f.write(key)
 		f.close()
 		print("Key saved to ~/.nd_config/key. Gathering your news now...")
+		time.sleep(5)
