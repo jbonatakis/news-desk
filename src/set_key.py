cbd083b4536b354f16bdfa49d47b639720895659
Jack Bonatakis <jackbonatakis@gmail.com>
2018-06-09 13:54:34 -0600

    Update newsapi.org link

--- src/set_key.py
+++ src/set_key.py
@@ -12,7 +12,7 @@ def set_key():
 		f.close()
 	except FileExistsError:
 		f = open(home + '/.nd_config/key', 'w')
-		print("Welcome to News Desk! You need an API key. Please point your browser to www.newsapi.org, register for a free developer account, and copy your API key.")
+                print("Welcome to News Desk! You need an API key. Please point your browser to https://newsapi.org, register for a free developer account, and copy your API key.")
 		print("Once you complete this process you will not need to repeat it unless you regenerate you API key or delete the key file.")
 		key = str(getpass.getpass("Enter newsapi.org key here >> ")).strip()
 		f.write(key)
